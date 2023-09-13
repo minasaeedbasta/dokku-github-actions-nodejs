d921a7baf9e4893a1fc9b59a232a4d351de7d40c
minasaiedbasta <mina.saied.basta@gmail.com>
2023-09-13 23:20:56 +0300

    add /users api resource

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+  res.send({
+    data : [
+      {
+        id:1,
+        name:"mina"
+      },
+      {
+        id:2,
+        name:"mariam"
+      }
+    ]
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
